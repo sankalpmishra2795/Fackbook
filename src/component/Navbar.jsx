51f3523f0fafaabed1d292bec35f92729f736b80
sankalpmishra2795 <sankalpmishra2795@gmail.com>
2020-10-27 22:40:19 +0530

    Navbar add

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Loginpage from '../component/Loginpage';
+import Profile from '../component/Profile';
+import {Route} from 'react-router-dom'
+
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <>
+                    <div className='mycol text-center'>
+                        <i class="fab fa-facebook-f fa-2x m-1" style={{}}></i>
+                        <span class="mb-0 h1" style={{}}>ackbook</span>
+                    </div>
+                <Route path='/' component={Loginpage} />
+                <Route path='/profile' component={Profile} />
+            </>
+        )
+    }
+}
